81a5a9c65ff94dd2d58984c946c045dfb33f8c86
Florin Godard <fgodard@gmail.com>
2013-11-03 19:36:15 +0000

    Added cron file that only one script needs to be called via cronjob

---
+++ cron.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# set to path where scripts are, usually /home/$USER/bin/otrAutoDownload
+otrAutoDownloadPath="/home/florin/bin/otrAutoDownload"
+
+source $otrAutoDownloadPath/otr.conf
+
+# grep new links
+$otrAutoDownloadPath/otrGrepLinks.sh $otrAutoDownloadPath
+
+# check if happy hour downloads are running
+endTime=$(($OTRENDHAPPYHOURHOUR*60+$OTRENDHAPPYHOURMINUTE))
+
+if [ -f "$otrAutoDownloadPath/happyHour.lock" ]
+then
+  # end downloads if shortly before end of happy hour
+  echo "It is $(env TZ=Europe/Berlin date +%H:%M), end script by $OTRENDHAPPYHOURHOUR:$OTRENDHAPPYHOURMINUTE"
+  currentTime=$(($(env TZ=Europe/Berlin date +%_H)*60+$(env TZ=Europe/Berlin date +%_M)))
+  #echo "$endTime vs. $currentTime"
+  if (( $currentTime > $endTime ))
+  then
+    echo "End script."
+    pid=$(cat $otrAutoDownloadPath/happyHour.lock)
+    $otrAutoDownloadPath/killTree.sh $pid
+    rm $otrAutoDownloadPath/happyHour.lock
+  else
+    echo "Script can run on."
+  fi
+else
+  # start downloads if in happy hour
+  echo "It is $(env TZ=Europe/Berlin date +%H:%M)h, start script between $OTRSTARTHAPPYHOURHOUR:$OTRSTARTHAPPYHOURMINUTE h and $OTRENDHAPPYHOURHOUR:$OTRENDHAPPYHOURMINUTE h"
+  startTime=$(($OTRSTARTHAPPYHOURHOUR*60+$OTRSTARTHAPPYHOURMINUTE))
+  currentTime=$(($(env TZ=Europe/Berlin date +%_H)*60+$(env TZ=Europe/Berlin date +%_M)))
+  #echo "$startTime vs. $currentTime"
+  if (( $currentTime > $startTime && $currentTime < $endTime ))
+  then
+    echo "Start script."
+    $otrAutoDownloadPath/otrHappyHour.sh $otrAutoDownloadPath &
+  else
+    echo "Don't start Happy Hour Script."
+  fi
+fi
+
+# decode new files
