08500eef26682955eec4310fe90a0fabdd3198f9
Florin Godard <fgodard@gmail.com>
2013-11-03 19:37:35 +0000

    Added script for decoding

---
+++ otrDecode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+        echo "Please call with ./otrDecode.sh configFolder fileName"
+        exit 1
+fi
+
+source $1/otr.conf
+echo "Decoding $2 with user $OTREMAIL into $OTROUTPUTPATH"
+$OTRQEMUPATH/bin/qemu-x86_64 -L $OTRQEMUPATH $OTRQEMUPATH/bin64/otrdecoder-64 -e $OTREMAIL -p $OTRPASSWORD -i $2 -o $OTROUTPUTPATH
