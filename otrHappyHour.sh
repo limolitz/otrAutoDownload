6ab2ab916530d13dde61a26f1d08e22fd552ddf8
Florin Godard <fgodard@gmail.com>
2013-11-03 19:39:04 +0000

    Added script for happy hour download

---
+++ otrHappyHour.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+        echo "Please call with ./otrHappyHour.sh configFolder"
+        exit 1
+fi
+
+
+tmpFile="/tmp/otr_happyHour_new.txt"
+lockFile="$1/happyHour.lock"
+
+source $1/otr.conf
+
+touch $lockFile
+echo $$ > $lockFile
+echo -n "" > $tmpFile
+
+uniq $OTRDIRECTLINKFILE | while read line; do
+   /bin/echo "Downloading $line"
+   /usr/bin/aria2c $line -d /media/hdd/aria2 -m 0 --retry-wait=30 --auto-file-renaming=false --on-download-complete= >/dev/null
+   download=$?
+   retry=false
+   case $download in
+   	0)
+				echo "$line downloaded."
+				;;
+    13)
+				echo "File already exists."
+				;;
+    18)
+     		echo "Error: aria2 could not create directory."
+        retry=true
+     		;;
+    *)
+        echo "Error: $line not downloaded. Unhandled status $download"
+        retry=true
+        ;;
+    esac
+    # if download file, add link again for next run
+    if $retry ; then
+      echo "Retrying $line"
+      echo $line >> $tmpFile
+    fi
+done
+
+/bin/rm $OTRDIRECTLINKFILE
+mv $tmpFile $OTRDIRECTLINKFILE
+/bin/rm $lockFile
