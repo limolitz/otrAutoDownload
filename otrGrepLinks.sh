c835c47624da10748980ba19b73eae0137f6a289
Florin Godard <fgodard@gmail.com>
2013-11-05 00:49:03 +0000

    Adjusted RegExp for more servers

--- otrGrepLinks.sh
+++ otrGrepLinks.sh
@@ -13,11 +13,11 @@ $1/dropboxUploader/dropbox_uploader.sh download $OTRDROPBOXPATH $inFile
 # Grep for torrent file as attachment
 /bin/grep -oP "filename=.*otrkey" $inFile | /bin/grep -oP "=.*" | /bin/grep -oP "[^=]*.otrkey$" >> $outFile
 # grep for torrent link
-/usr/bin/tr -d '=\n' < $inFile | /bin/grep -oP '"http://81.95.11.2.*xbt_torrent_create.php[^"]*[a-z]"' | /bin/grep -oP "3D.*otrkey" | /bin/grep -oP '[^3D].*' >> $outFile
+/usr/bin/tr -d '=\n' < $inFile | /bin/grep -oP '"http://.*xbt_torrent_create.php[^"]*[a-z]"' | /bin/grep -oP "3D.*otrkey" | /bin/grep -oP '[^3D].*' >> $outFile
 # grep for direct download link for happy hour download
-/bin/grep -oP "http://81.95.11.22.*.otrkey" $inFile >> $1/otrHappyHourLinks.txt
+/bin/grep -oP "http://[^>]*(otrkey|avi|mp4){1}" $inFile >> $1/otrHappyHourLinks.txt
 # grep for filename from direct download link for torrent download
-/bin/grep -oP "http://81.95.11.22.*.otrkey" $inFile | grep -oP "[^/]*otrkey" >> $outFile
+/bin/grep -oP "http://[^>]*(otrkey){1}" $inFile | grep -oP "[^/]*otrkey" >> $outFile
 cat $outFile | while read line; do
    # grep date from file
    date=$(/bin/echo $line | egrep -o "[0-9]{2}\.[0-9]{2}\.[0-9]{2}")
