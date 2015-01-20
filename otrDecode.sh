008e80a089fd90cf8a06c039013bcc74c92967cf
Florin Godard <fgodard@gmail.com>
2015-01-20 20:29:29 +0000

    Adjusted logging

--- otrDecode.sh
+++ otrDecode.sh
@@ -6,5 +6,6 @@ fi
 
 source $1/otr.conf
 echo "Decoding $2 with user $OTREMAIL into $OTROUTPUTPATH"
-$OTRQEMUPATH/bin/qemu-x86_64 -L $OTRQEMUPATH $OTRQEMUPATH/bin64/otrdecoder-64 -e $OTREMAIL -p $OTRPASSWORD -i $2 -o $OTROUTPUTPATH
+# Output can be discarded as we handle errors with the exit code (at least in otrDecodeAllFiles.sh)
+$OTRQEMUPATH/bin/qemu-x86_64 -L $OTRQEMUPATH $OTRQEMUPATH/bin64/otrdecoder-64 -e $OTREMAIL -p $OTRPASSWORD -i $2 -o $OTROUTPUTPATH >/dev/null 2>/dev/null
 exit $?
