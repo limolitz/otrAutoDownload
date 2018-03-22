3c513e38621be7ef607abc3888005fcff8db8ce5
Florin Godard <fgodard@gmail.com>
2018-03-22 14:17:54 +0000

    Switch to otrpidecoder

--- otrDecode.sh
+++ otrDecode.sh
@@ -6,6 +6,5 @@ fi
 
 source $1/otr.conf
 echo "Decoding $2 with user $OTREMAIL into $OTROUTPUTPATH"
-# Output can be discarded as we handle errors with the exit code (at least in otrDecodeAllFiles.sh)
-$OTRQEMUPATH/bin/qemu-x86_64 -L $OTRQEMUPATH $OTRQEMUPATH/bin64/otrdecoder-64 -e $OTREMAIL -p $OTRPASSWORD -i $2 -o $OTROUTPUTPATH >/dev/null 2>/dev/null
+/home/florin/bin/otrpidecoder -d -e $OTREMAIL -p $OTRPASSWORD -D $OTROUTPUTPATH $2 2>&1
 exit $?
